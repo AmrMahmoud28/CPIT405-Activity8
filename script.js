cb6c1069f3c821bad043652a761d73c152dd23a6
AmrMahmoud <dramr2852001@gmail.com>
2024-10-23 03:05:20 +0300

    added create time table function

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const startTimeSelectElement = document.getElementById('start-time');
-const endTimeSelectElement = document.getElementById('end-time');
+const startTimeSelectElement = document.getElementById("start-time");
+const endTimeSelectElement = document.getElementById("end-time");
 
 let startHour = 8;
 let endHour = 17;
 
 const populateDropDownMenu = (selectElement, selectedValue) => {
-    for (let i = 0; i< 24; i++) {
-        const optionElement = document.createElement('option');
-        
+    for (let i = 0; i < 24; i++) {
+        const optionElement = document.createElement("option");
+
         let hour = i % 12 === 0 ? 12 : i % 12;
-        hour += ':00';
-        hour += i < 12 ? ' AM' : ' PM';
+        hour += ":00";
+        hour += i < 12 ? " AM" : " PM";
         optionElement.value = i;
         optionElement.text = hour;
 
@@ -19,15 +19,52 @@ const populateDropDownMenu = (selectElement, selectedValue) => {
         }
         selectElement.appendChild(optionElement);
     }
-}
+};
 
 populateDropDownMenu(startTimeSelectElement, 8);
 populateDropDownMenu(endTimeSelectElement, 17);
 
-startTimeSelectElement.addEventListener('change', (event) => {
+startTimeSelectElement.addEventListener("change", (event) => {
     startHour = parseInt(event.target.value);
+    createTimeTable();
 });
 
-endTimeSelectElement.addEventListener('change', (event) => {
+endTimeSelectElement.addEventListener("change", (event) => {
     endHour = parseInt(event.target.value);
+    createTimeTable();
 });
+
+const createTimeTable = () => {
+    const tableElement = document.getElementById("timeTable");
+    const days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+    ];
+
+    let tableHtml = "<table><thead><tr><th></th>";
+    days.forEach((day) => (tableHtml += `<th class="day-header">${day}</th>`));
+
+    tableHtml += "</tr></thead><tbody>";
+
+    for (let i = startHour; i <= endHour; i++) {
+        let hour = i % 12 === 0 ? 12 : i % 12;
+        hour += ":00";
+        hour += i < 12 ? " AM" : " PM";
+
+        tableHtml += `<tr><td class="time-label">${hour}</td>`;
+        days.forEach(
+            (day) =>
+                (tableHtml += `<td class="time-slot" onclick="toggleTimeSlot(this)" data-day="${day}" data-time="${hour}"></td>`)
+        );
+        tableHtml += "</tr>";
+    }
+
+    tableHtml += "</tbody></table>";
+    tableElement.innerHTML = tableHtml;
+};
+
+createTimeTable();
